eb7f67bbf1c67c661bd96fba00a09a4ad7b696ce
DHKIM <kdh30400@naver.com>
2017-08-04 14:22:48 +0900

    [도형] 로그인 화면 작업중

---
+++ app/src/main/java/com/example/mjk/jjamtalk/Data/MyData.java
@@ -0,0 +1,34 @@
+package com.example.mjk.jjamtalk.Data;
+
+/**
+ * Created by boram on 2017-08-04.
+ */
+
+public class MyData {
+
+    private static MyData  _Instance;
+
+    public static MyData getInstance()
+    {
+        if(_Instance == null)
+            _Instance = new MyData();
+
+        return  _Instance;
+    }
+
+    private String UserIdx;
+
+
+    private MyData()
+    {
+
+    }
+
+    public void setUserIdx(String userIdx) {
+        UserIdx = userIdx;
+    }
+
+    public String getUserIdx() {
+        return UserIdx;
+    }
+}
---
+++ app/src/main/java/com/example/mjk/jjamtalk/InputProfile.java
@@ -0,0 +1,13 @@
+package com.example.mjk.jjamtalk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InputProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_profile);
+    }
+}
